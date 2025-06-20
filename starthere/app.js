67f8ee9a83304f57c42416b205207fc4efd45ace
Ananya <ananyajoshi137@gmail.com>
2025-06-20 23:57:12 +0000

    [automatic commit] 2025-06-20 23:57:11 UTC 101.115.227.20

--- starthere/app.js
+++ starthere/app.js
@@ -98,7 +98,7 @@ let db;
       INSERT INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
         ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
-        ((SELECT user_id FROM Users WHERE username='eveowner'), 'Rocky', 'large'),
+        ((SELECT user_id FROM Users WHERE username='serenaowner'), 'Rocky', 'large'),
         ((SELECT user_id FROM Users WHERE username='alice123'), 'Luna', 'small'),
         ((SELECT user_id FROM Users WHERE username='carol123'), 'Charlie', 'medium');
 
