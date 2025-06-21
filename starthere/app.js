5576b0ecf777ca6d103db9988b135dbfd6026a28
Ananya <ananyajoshi137@gmail.com>
2025-06-21 00:02:59 +0000

    [automatic commit] 2025-06-21 00:02:59 UTC 101.115.227.20

--- starthere/app.js
+++ starthere/app.js
@@ -96,8 +96,8 @@ let db;
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('walker', 'david@example.com', 'hashed321', 'walker'),
-        ('eveowner', 'eve@example.com', 'hashed654', 'owner');
+        ('blarewalker', 'david@example.com', 'hashed351', 'walker'),
+        ('serenaowner', 'eve@example.com', 'hashed654', 'owner');
 
       INSERT INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
