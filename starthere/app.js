165564e5072db12e49944b0713f2e43ba187fa14
Ananya <ananyajoshi137@gmail.com>
2025-06-21 06:13:57 +0000

    [automatic commit] 2025-06-21 06:13:57 UTC 120.20.80.85

--- starthere/app.js
+++ starthere/app.js
@@ -108,8 +108,8 @@ let db;
         ((SELECT dog_id FROM Dogs WHERE name='Max'),'2025-06-10 08:00:00',30,'Parklands','open'),
         ((SELECT dog_id FROM Dogs WHERE name='Bella'),'2025-06-10 09:30:00',45,'Beachside Ave','accepted'),
         ((SELECT dog_id FROM Dogs WHERE name='Rocky'),'2025-06-11 10:00:00',60,'City Park','completed'),
-        ((SELECT dog_id FROM Dogs WHERE name='Coco'),'2025-06-12 07:30:00',20,'Hilltop Trail','open'),
-        ((SELECT dog_id FROM Dogs WHERE name='Milo'),'2025-06-13 14:00:00',40,'River Walk','cancelled');
+        ((SELECT dog_id FROM Dogs WHERE name='Luna'),'2025-06-12 07:30:00',20,'Hilltop Trail','open'),
+        ((SELECT dog_id FROM Dogs WHERE name='Charlie'),'2025-06-13 14:00:00',40,'River Walk','cancelled');
     `);
 
     await db.execute(`
